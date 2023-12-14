faae23083a56c516c285cdbe7cb90349ae4024a4
trixiamarie <trixialorenzana@gmail.com>
2023-12-14 17:30:51 +0100

    Bar Laterale

---
+++ js/homepage.js
@@ -0,0 +1,28 @@
+let api= "https://striveschool-api.herokuapp.com/api/deezer/search?q={no}";
+
+fetch(api, {
+    method: "GET",
+    
+})
+.then((response)=> response.json())
+.then((data)=>{
+    console.log(data);
+    listsongs(data);
+})
+.catch((error) =>{
+    console.error("Errore nella richiesta:", error);
+});
+
+function listsongs(song){
+    console.log(song.data);
+    song.data.forEach((element) => {
+       let div = document.querySelector('#listsongs');
+       let a =document.createElement('a');
+       a.href = element.link;
+       let p = document.createElement('p');
+       p.innerText = element.title;
+       div.appendChild(a);
+       a.appendChild(p);
+    });
+}
+
