95b452f4779ffae1993c90fe4af13eec389a642b
trixiamarie <trixialorenzana@gmail.com>
2023-12-18 18:33:46 +0100

    edit

--- js/album.js
+++ js/album.js
@@ -1,5 +1,22 @@
 let api= "https://striveschool-api.herokuapp.com/api/deezer/search?q={no}";
 
+let params = new URLSearchParams(document.location.search);
+let id = params.get("id");
+console.log("ID from URL:", id);
+
+//LOCAL STORAGE PER ALBUM SALVATI
+let storedAlbums = localStorage.getItem('AlbumLiked');
+console.log('Stored Albums:', storedAlbums);
+let arrayHeartAlbum = storedAlbums !== null ? JSON.parse(storedAlbums) : [];
+console.log('Parsed Array Albums:', arrayHeartAlbum);
+
+//LOCAL STORAGE PER ARTISTI SALVATI
+let storedArtist = localStorage.getItem('ArtistLiked');
+console.log('Stored Artist:', storedArtist);
+let arrayHeartArtist = storedArtist !== null ? JSON.parse(storedArtist) : [];
+console.log('Parsed Array Artist:', arrayHeartArtist);
+
+//SIDEBAR FETCH
 fetch(api, {
     method: "GET",
     
@@ -13,23 +30,162 @@ fetch(api, {
     console.error("Errore nella richiesta:", error);
 });
 
+//SIDEBAR SINISTRO SEARCH
+let searchBtn = document.querySelectorAll('.searchElement');
+searchBtn.forEach((x) =>{
+    x.addEventListener('click', function() {
+        const input = document.querySelector('#searchBar');
+        input.classList.toggle('hidden');
+        input.focus();
+    })}
+)
+
+
+let btnSearch = document.querySelector('#btnSearch');
+btnSearch.addEventListener('click', function(){
+    let searchValue = document.querySelector('#searchValue').value;
+    window.location = `./artistpage.html?artist=${searchValue}`;
+    console.log(searchValue);
+})
+document.querySelector('#searchValue').addEventListener('keypress', function(){
+    
+    let searchValue = document.querySelector('#searchValue').value;
+    window.location = `./artistpage.html?artist=${searchValue}`;
+    console.log(searchValue);
+    // console.log(e.key);
+})
+
+//LISTA ALBUM SALVATI
+let btnAlbum = document.querySelector('#btnAlbum');
+btnAlbum.addEventListener('click',  function () {
+    let div = document.querySelector('#listsongs');
+    div.innerHTML = '';
+    let divSearch = document.createElement('div');
+    divSearch.classList.add('d-flex', 'justify-content-between', 'align-items-center');
+    divSearch.innerHTML = `<i class="bi bi-search"></i>
+    <div class="d-flex align-items-baseline">
+      <p class="m-0 p-2">Recenti</p>
+      <i class="bi bi-list-ul fs-4 mt-auto me-3"></i>
+    </div>`
+
+    div.appendChild(divSearch);
+    arrayHeartAlbum.forEach((element) => {
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${element}`, {
+            method: "GET",
+        })
+        .then((response)=> response.json())
+        .then((data)=>{
+            console.log(data);
+            let div = document.querySelector('#listsongs');
+            let a =document.createElement('a');
+            a.href = `./album.html?id=${data.id}`;
+            let p = document.createElement('p');
+            p.innerText = data.title;
+            div.appendChild(a);
+            a.appendChild(p);
+    })
+        .catch((error) =>{
+            console.error("Errore nella richiesta:", error);
+        });
+
+     });
+    
+});
+
+//LISTA ARTISTI SALVATI
+let btnArtist = document.querySelector('#btnArtist');
+btnArtist.addEventListener('click', function () {
+    let div = document.querySelector('#listsongs');
+    div.innerHTML = '';
+    let divSearch = document.createElement('div');
+    divSearch.classList.add('d-flex', 'justify-content-between', 'align-items-center');
+    divSearch.innerHTML = `<i class="bi bi-search"></i>
+    <div class="d-flex align-items-baseline">
+      <p class="m-0 p-2">Recenti</p>
+      <i class="bi bi-list-ul fs-4 mt-auto me-3"></i>
+    </div>`
+    div.appendChild(divSearch);
+
+    arrayHeartArtist.forEach((element) => {
+        fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${element}`, {
+                method: "GET",
+            })
+            .then((response)=> response.json())
+            .then((data)=>{
+                console.log(data);
+                let div = document.querySelector('#listsongs');
+                let a =document.createElement('a');
+                a.href = `./artistpage.html?id=${data.id}`;
+                let p = document.createElement('p');
+                p.innerText = data.name;
+                div.appendChild(a);
+                a.appendChild(p);
+        })
+            .catch((error) =>{
+                console.error("Errore nella richiesta:", error);
+            });
+    
+         });
+});
+
+//LISTA PLAYLIST AL CLICK
+let btnPlaylist = document.querySelector('#btnPlaylist');
+btnPlaylist.addEventListener('click', function () {
+    let div = document.querySelector('#listsongs');
+    div.innerHTML = '';
+    let divSearch = document.createElement('div');
+    divSearch.classList.add('d-flex', 'justify-content-between', 'align-items-center');
+    divSearch.innerHTML = `<i class="bi bi-search"></i>
+    <div class="d-flex align-items-baseline">
+      <p class="m-0 p-2">Recenti</p>
+      <i class="bi bi-list-ul fs-4 mt-auto me-3"></i>
+    </div>`
+
+    div.appendChild(divSearch);
+
+    fetch(api, {
+        method: "GET",
+        
+    })
+    .then((response)=> response.json())
+    .then((data)=>{
+        console.log(data);
+        listsongs(data);
+    })
+    .catch((error) =>{
+        console.error("Errore nella richiesta:", error);
+    });
+});
+
+//LISTA PLAYLIST AL CARICAMENTO DELLA PAGINA
 function listsongs(song){
+    
     console.log(song.data);
     song.data.forEach((element) => {
-       let div = document.querySelector('#listsongs');
-       let a =document.createElement('a');
+       let div = document.querySelectorAll('.listsongs');
+       div.forEach((div)=>{
+       let a = document.createElement('a');
        a.href = element.link;
        let p = document.createElement('p');
        p.innerText = element.title;
        div.appendChild(a);
-       a.appendChild(p);
-    });
+       a.appendChild(p);}
+    )});
 }
 
-let params = new URLSearchParams(document.location.search);
-let id = params.get("id");
-console.log("ID from URL:", id);
+//SIDEBAR ATTIVITÀ AMICI
+document.getElementById('toggleBtn').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar2');
+    sidebar.classList.toggle('hidden');
+});
+  
+document.getElementById('closeBtn').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar2');
+    sidebar.classList.add('hidden');
+});
+  
 
+//SEZIONE ALBUM
 fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`, {
     method: "GET",
 })
@@ -40,16 +196,36 @@ fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`, {
     createContentAlbum(data.tracks.data);
     console.log(data.tracks.data);
     document.title = (data.title)
-})
+
+    let heart = document.querySelector('#AddAlbumToLiked');
+    
+    heart.addEventListener('click', function (){
+        console.log(arrayHeartAlbum);
+        if (!arrayHeartAlbum.includes(data.id)) {
+            
+            arrayHeartAlbum.push(data.id);
+            
+            localStorage.setItem('AlbumLiked', JSON.stringify(arrayHeartAlbum));
+            console.log(data.id);
+            
+        } else {
+            console.log("Album already in the liked list");
+            const indexToRemove = arrayHeartAlbum.indexOf(data.id);
+
+    
+    if (indexToRemove !== -1) {
+        arrayHeartAlbum.splice(indexToRemove, 1);
+        localStorage.setItem('AlbumLiked', JSON.stringify(arrayHeartAlbum));
+        console.log(`Removed album id ${data.id} from liked list`);
+    }
+            }})
+        }
+    )
 .catch((error) =>{
     console.error("Errore nella richiesta:", error);
 });
 
-
-//SEZIONE ALBUM
-
-
-
+//CREA PARTE SUPERIORE ALBUM
 function creaAlbum(id) {
     let albumContainer = document.querySelector('#imgalbumcontainer');
     let albumContainer1 = document.querySelector('#albumcontainer');
@@ -63,14 +239,16 @@ function creaAlbum(id) {
     titleAlbum.innerText = id.title;
     tdalbum.appendChild(titleAlbum);
     let artist = document.createElement('h5');
-    artist.innerHTML = `<img src="${id.artist.picture}" style="height: 1.5rem; width: 1.5rem; border-radius: 50%"><b>${id.artist.name}</b> • ${id.release_date} • ${id.nb_tracks} brani, ${Math.floor(id.duration/60)} min, ${Math.floor(id.duration%60)} sec.`;
+    artist.innerHTML = `<img src="${id.artist.picture}" style="height: 1.5rem; width: 1.5rem; border-radius: 50%"><a href="./artistpage.html?id=${id.artist.id}" class="text-decoration-none" style="color: white" ><b>${id.artist.name}</b></a> • ${id.release_date} • ${id.nb_tracks} brani, ${Math.floor(id.duration/60)} min, ${Math.floor(id.duration%60)} sec.`;
     tdalbum.appendChild(artist);
 }
 
+//CREA TRACKLIST
 function createContentAlbum(id){
     let i = 1;
 
     id.forEach(x => {
+        console.log(x);
         let contentAlbum = document.querySelector('#contentalbum');
         let row = document.createElement('div');
         row.classList.add('row');
@@ -82,11 +260,15 @@ function createContentAlbum(id){
         index.innerText = i;
 
         let col10 = document.createElement('div');
-        col10.classList.add('col-10');
-        let tSong = document.createElement('p');
+        col10.classList.add('col-10','d-flex', 'flex-column' );
+        let tSong = document.createElement('a');
+        tSong.classList.add('text-decoration-none');
+        tSong.href = x.link;
         tSong.style.color="#e9e9e9";
-        let tArtist = document.createElement('p');
+        let tArtist = document.createElement('a');
         tArtist.style.color="#828282";
+        tArtist.href= `./artistpage.html?id=${x.artist.id}`;
+        tArtist.classList.add('text-decoration-none');
         tSong.innerText= x.title;
         tArtist.innerText = x.artist.name;
 
@@ -105,23 +287,10 @@ function createContentAlbum(id){
         row.appendChild(col1pt2);
         col1pt2.appendChild(durata);
 
+       
+
         
         i++
     });
 }
 
-
-
-
-//sidebar
-document.getElementById('toggleBtn').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar2');
-    sidebar.classList.toggle('hidden');
-  });
-  
-  
-  document.getElementById('closeBtn').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar2');
-    sidebar.classList.add('hidden');
-  });
-  
